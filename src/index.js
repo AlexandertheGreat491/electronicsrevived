8d9f1894308be47cf8d4ecc2e51443e3253aed96
agvan <agvandyke25@gmail.com>
2025-07-26 14:44:27 -0500

    added importing for button, input, browserrouter and the react-router-dom dependency

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import React,{useState, useEffect} from "react";
+import {Card, CardContent} form 
+"@/components/ui/card"; import { Button } from
+"@/components/ui/button"; import { Input } from
+"@/components/ui/input"; import { BrowserRouter as Router,Routes, Route, Link, useNavigate } from
+"react-router-dom";
